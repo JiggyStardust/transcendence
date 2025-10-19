664b1008b71e05749719ec3586aea063957e9c0c
ipersids <ipersids@student.hive.fi>
2025-10-19 13:38:45 +0300

    [frontend] Fix .env loading in vite.config.js

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-const getEnv = (key: string, defaultValue?: string): string => {
-  const value = process.env[key] || defaultValue;
-  if (!value) {
-    throw new Error(`❌ Missing required environment variable: ${key}`);
-  }
-  return value;
-};
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: getEnv("VITE_HOST", "0.0.0.0"),
-    port: parseInt(getEnv("VITE_PORT", "3000")),
-    proxy: {
-      "/api": {
-        target: getEnv("VITE_BACKEND_INTERNAL_URL"),
-        changeOrigin: true,
+  const getEnv = (key: string, defaultValue?: string): string => {
+    const value = process.env[key] || defaultValue;
+    if (!value) {
+      throw new Error(`❌ Missing required environment variable: ${key}`);
+    }
+    return value;
+  };
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      host: getEnv("VITE_HOST", "0.0.0.0"),
+      port: parseInt(getEnv("VITE_PORT", "3000")),
+      proxy: {
+        "/api": {
+          target: getEnv("VITE_BACKEND_INTERNAL_URL"),
+          changeOrigin: true,
+        },
       },
     },
-  },
+  }
 });
