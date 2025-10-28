b9e645488b7649c86ec448f291834e5eb4676f49
JiggyStardust <saaraleni@gmail.com>
2025-10-28 15:54:36 +0200

    Started to create bg-color and btn layout

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -17,7 +17,7 @@ export const Button: React.FC<ButtonProps> = ({
   const navigate = useNavigate();
 
   const baseStyles =
-    "px-6 py-3 rounded-xl font-semibold transition-colors duration-200";
+    "px-26 py-2 rounded-[10px] font-[Chango] transition-colors duration-200 border-4 border-transparent"; // px = padding horizontal, py = padding vertical
   const variants = {
     primary: "bg-blue-600 text-white hover:bg-blue-700",
     secondary: "bg-white text-blue-700 hover:bg-gray-200",
@@ -28,8 +28,20 @@ export const Button: React.FC<ButtonProps> = ({
     else if (onClick) onClick();
   };
 
+  // Our styling comes from index.css
+
+    const style: React.CSSProperties = {
+    backgroundColor: "var(--btn-bg)",
+    color: "var(--btn-text)",
+    borderImage: "linear-gradient(to right, var(--btn-border-start), var(--btn-border-end)) 1",
+  };
+
   return (
-    <button className={`${baseStyles} ${variants[variant]}`} onClick={handleClick}>
+    <button
+      style={style}
+      className={baseStyles + " hover:bg-[var(--btn-hover)]"}
+      onClick={handleClick}
+    >
       {children}
     </button>
   );
