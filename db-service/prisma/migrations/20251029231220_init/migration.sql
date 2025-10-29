3bbb9cc68deafc7aea08cf806467fb38cdfc770d
Julia Llama <julia.persidskaia@gmail.com>
2025-10-30 01:20:39 +0200

    Add prisma orm and database schema

---
+++ db-service/prisma/migrations/20251029231220_init/migration.sql
@@ -0,0 +1,107 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "login" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "isTwoFactorEnabled" BOOLEAN NOT NULL DEFAULT false,
+    "avatarID" INTEGER,
+    "status" TEXT NOT NULL,
+    "updatedAt" DATETIME NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "User_avatarID_fkey" FOREIGN KEY ("avatarID") REFERENCES "Avatar" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "RefreshToken" (
+    "uuid" TEXT NOT NULL PRIMARY KEY,
+    "userId" INTEGER NOT NULL,
+    "hashedToken" TEXT NOT NULL,
+    "expiresAt" DATETIME NOT NULL,
+    "revokedAt" DATETIME,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "RefreshToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Avatar" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "link" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "uploadedBy" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Avatar_uploadedBy_fkey" FOREIGN KEY ("uploadedBy") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Friend" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userID" INTEGER NOT NULL,
+    "friendID" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Friend_userID_fkey" FOREIGN KEY ("userID") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Friend_friendID_fkey" FOREIGN KEY ("friendID") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Tournament" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "createdBy" INTEGER NOT NULL,
+    "startDate" DATETIME NOT NULL,
+    "endDate" DATETIME NOT NULL,
+    CONSTRAINT "Tournament_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Match" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "tournamentId" INTEGER,
+    "winnerId" INTEGER,
+    "startedAt" DATETIME,
+    "endedAt" DATETIME,
+    CONSTRAINT "Match_tournamentId_fkey" FOREIGN KEY ("tournamentId") REFERENCES "Tournament" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "Match_winnerId_fkey" FOREIGN KEY ("winnerId") REFERENCES "User" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "MatchParticipant" (
+    "matchId" INTEGER NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "score" INTEGER NOT NULL DEFAULT 0,
+    "isWinner" BOOLEAN NOT NULL DEFAULT false,
+    "startedAt" DATETIME,
+    "endedAt" DATETIME,
+
+    PRIMARY KEY ("matchId", "userId"),
+    CONSTRAINT "MatchParticipant_matchId_fkey" FOREIGN KEY ("matchId") REFERENCES "Match" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "MatchParticipant_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_login_key" ON "User"("login");
+
+-- CreateIndex
+CREATE INDEX "RefreshToken_userId_idx" ON "RefreshToken"("userId");
+
+-- CreateIndex
+CREATE INDEX "RefreshToken_expiresAt_idx" ON "RefreshToken"("expiresAt");
+
+-- CreateIndex
+CREATE INDEX "Friend_friendID_idx" ON "Friend"("friendID");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Friend_userID_friendID_key" ON "Friend"("userID", "friendID");
+
+-- CreateIndex
+CREATE INDEX "Tournament_createdBy_idx" ON "Tournament"("createdBy");
+
+-- CreateIndex
+CREATE INDEX "Match_tournamentId_idx" ON "Match"("tournamentId");
+
+-- CreateIndex
+CREATE INDEX "Match_winnerId_idx" ON "Match"("winnerId");
+
+-- CreateIndex
+CREATE INDEX "MatchParticipant_userId_idx" ON "MatchParticipant"("userId");
