415d85efdc3205443ec8ed5b23b69d29d90510c9
mariaaallotar <maria.helenius99@gmail.com>
2025-10-30 14:55:43 +0200

    Minimal input validation for SignUp

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -4,9 +4,11 @@ type InputProps = {
   id: string;
   label: string;
   type?: string;
+  value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
 };
 
-export default function Input({id, label, type = "text"}: InputProps) {
+export default function Input({id, label, type = "text", value, onChange}: InputProps) {
 
 	return (
 		<div className="flex flex-col m-2 px-4">
@@ -14,7 +16,7 @@ export default function Input({id, label, type = "text"}: InputProps) {
 				{label}
 			</label>
 			<input className="p-1 border border-white rounded-sm"
-				type={type} id={id}/>
+				type={type} id={id} value={value} onChange={onChange}/>
 		</div>
 	)
 }
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,24 +1,54 @@
-import { useState, useMemo } from "react";
-import Input from "../components/Input"
+import { useState } from "react";
+import Input from "../components/Input";
+import { Button} from "../components/Button";
 
 export default function SignUp() {
 	
 	const [information, setInformation] = useState("");
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-	function handleSignUp({username, password: string}) {
-		if (username )
+	function handleSignUp() {
 		//API call here
 		console.log("Sign up API called");			//TEST
-		setInformation("username taken");
 	}
 
+	function usernameInUse() {
+		//API call here
+		return (true);			//Based on reply
+	}
+
+	function handleUsernameChange(e) {
+		const username = e.target.value;
+		console.log(username);
+		setUsername(username);
+		if (username === "") {
+			setInformation("Username can not be empty");
+		}
+		else if (usernameInUse()) {
+			setInformation("Username already taken");
+		} else {
+			setInformation("");
+		}
+	}
+
+	function handlePasswordChange(e) {
+		const password = e.target.value;
+		setPassword(password);
+		console.log(password);
+		if (password === "") {
+			setInformation("Password can not be empty");
+		} else {
+			setInformation("");
+		}
+	}
 
 	return (
 		<div>
-			<form action={handleSignUp} className="px-4 py-8 border border-white ">
+			<form action={handleSignUp} className="px-4 py-8 border border-white rounded-lg ">
 				<p>{information}</p>
-				<Input label="Username:" id="username" type="text" />
-				<Input id="password" label="Password:" type="text"/>
+				<Input id="username" label="Username:" type="text" value={username} onChange={handleUsernameChange}/>
+				<Input id="password" label="Password:" type="password" value={password} onChange={handlePasswordChange}/>
 				<button type="submit">Sign up</button>
 			</form>
 		</div>
