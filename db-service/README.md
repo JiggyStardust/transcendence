f7ccc0819f6f4b1c88eb862d1deceade44669b01
Julia Llama <julia.persidskaia@gmail.com>
2025-11-01 21:51:59 +0200

    Setup NestJS and Prisma project

---
+++ db-service/README.md
@@ -0,0 +1,1 @@
+# Database microservice REST API
---
+++ db-service/src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: { level: 'error' } }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Pong DB')
+    .setDescription('REST API documentation for Pong database.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('db-api', app, document);
+
+  await app.listen(process.env.PORT ?? 8082, process.env.HOST ?? '0.0.0.0');
+}
+bootstrap();
