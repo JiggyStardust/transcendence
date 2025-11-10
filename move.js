621011021c81d4f1544b422bb4aacade5177623f
Ethan <ethanberko@gmail.com>
2025-11-10 13:45:41 +0200

    collision now has its own function

--- move.js
+++ move.js
@@ -11,12 +11,21 @@ const zStartingAngle = vzStraightAngle;
 const xStartingPosition = 0;
 const zStartingPosition = 0;
 
+const paddleCollisionX = 2.4;
+const scoreCollisionX = 2.6;
+const sideCollisionZ = 2.6;
+const paddleSize = 1.5;
+const paddleCollisionSize = 0.95;
+
 const startingBallSpeed = 2.5;
 const ballSpeed = startingBallSpeed;
 const ballSpeedIncrement = 0.08;
 const maxBallSpeed = 5;
 
-export const movement = {
+let direction = new BABYLON.Vector3(xStartingAngle, 0, zStartingAngle);
+
+
+export const move = {
 	vxSmallAngle,
 	vxLargeAngle,
 	vxStraightAngle,
@@ -30,5 +39,11 @@ export const movement = {
 	startingBallSpeed,
 	ballSpeed,
 	ballSpeedIncrement,
-	maxBallSpeed
+	maxBallSpeed,
+	paddleCollisionX,
+	scoreCollisionX,
+	sideCollisionZ,
+	paddleSize,
+	paddleCollisionSize,
+	direction
 };
