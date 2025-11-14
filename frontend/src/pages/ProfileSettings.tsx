939a1a3d46c34facacd5bbb973ff97c438cc37cd
mariaaallotar <maria.helenius99@gmail.com>
2025-11-14 14:00:20 +0200

    Extracted profilePic/default icon to own component

--- frontend/src/pages/ProfileSettings.tsx
+++ frontend/src/pages/ProfileSettings.tsx
@@ -2,10 +2,28 @@ import { Button } from "../components/Button";
 import { FiUser } from "react-icons/fi";
 import Input from "../components/Input";
 
+const ProfilePic = () => {
+
+	return (
+		<div className="rounded-full border-4 p-2 w-[120px] h-[120px] flex items-center justify-center overflow-hidden">
+		  {/* {user?.image ? (
+		    <img
+		      src={user.image}
+		      alt="Profile"
+		      className="w-full h-full object-cover rounded-full"
+		    />
+		  ) : ( */}
+		    <FiUser size={120} />
+		  {/* )} */}
+		</div>
+	)
+}
+
 const ProfileInfo = ({}) => {
+
 	return (
 		<div className="flex gap-8">
-			<div className="rounded-full border-4 p-2"><FiUser size="120"/></div>
+			<ProfilePic />
 			<div className="flex flex-col gap-4 pt-4">
 				<h1 className="font-bold text-3xl">Username</h1>
 				<p>Some text</p>
@@ -28,8 +46,8 @@ const Settings = ({}) => {
 const ProfileSettings = ({}) => {
 
 	return (
-		<div className="flex justify-center">
-			<div className="flex flex-col gap-16 w-3/5 min-w-lg border-2">
+		<div className="flex justify-center pt-10">
+			<div className="flex flex-col gap-16 w-3/5 min-w-xl max-w-6xl">
 				<ProfileInfo />
 				<Settings />
 				<div className="flex justify-end">
