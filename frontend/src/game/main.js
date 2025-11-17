e4867f1383dbce7cbb73b33b4c68feceaaf77047
Ethan <ethanberko@gmail.com>
2025-11-17 15:04:56 +0200

    pulled main

--- frontend/src/game/main.js
+++ frontend/src/game/main.js
@@ -1,13 +1,13 @@
-import {game} from "./game.js";
+import {game} from "../frontend/src/game/game.js";
 import {parseUsername} from "./parseUsername.js";
-import {createScene} from "./createScene.js";
-import {applyCollision} from "./applyCollision.js";
+import {createScene} from "../frontend/src/game/createScene.js";
+import {applyCollision} from "../frontend/src/game/applyCollision.js";
 import {moveSphere} from "./moveSphere.js";
 import {moveCamera} from "./moveCamera.js";
 import {updateScoreText} from "./updateScoreText.js";
 import {pointScored} from "./pointScored.js";
 import {reset} from "./reset.js";
-import {gameOver} from "./gameOver.js";
+import {gameOver} from "../frontend/src/game/gameOver.js";
 
 // Init
 game.canvas = document.getElementById('renderCanvas');
