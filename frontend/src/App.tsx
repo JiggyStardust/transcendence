38b9dc3615abdc6dbbe477335c93b3d9504ce308
JiggyStardust <saaraleni@gmail.com>
2025-11-18 16:08:44 +0200

    Changed name SignIn to Login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
-import SignIn from "./pages/SignIn";
+import  from "./pages/";
 import SignUp from "./pages/SignUp";
 import Testing from "./pages/Testing";
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
 
 
 function App() {
@@ -14,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
+          <Route path="/login" element={<Login />} />
 	    	  <Route path="/testing" element={<Testing />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
