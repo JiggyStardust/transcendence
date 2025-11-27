f68a510926d5ed9281393c6ad5ab56d1a71fff1e
mariaaallotar <maria.helenius99@gmail.com>
2025-11-27 14:37:10 +0200

    BG color change on theme toggle

--- frontend/src/components/ThemeToggle.tsx
+++ frontend/src/components/ThemeToggle.tsx
@@ -14,7 +14,7 @@ export const ThemeToggle: React.FC = () => {
 	}
 
   return (
-    <button onClick={ changeTheme } className="flex gap-2 px-4 py-2 rounded-md bg-gray-200 dark:bg-gray-700 cursor-pointer">
+    <button onClick={ changeTheme } className="flex gap-2 px-4 py-2 rounded-md bg-stone-300 dark:bg-stone-600 cursor-pointer">
 			<span className="content-center" >{isDark ? <FiSun /> : <FiMoon />}</span>
 			<span>{isDark ? "Switch to light theme" : "Switch to dark theme"}</span>
     </button>
