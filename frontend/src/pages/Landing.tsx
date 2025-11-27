873dcdaa694adbc1a786ffabd1ab0ba2bcea0970
Ethan <ethanberko@gmail.com>
2025-11-27 11:00:46 +0200

    removed welcome to

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -8,11 +8,10 @@ export default function Landing() {
     <div className="flex flex-col gap-8 p-16 items-center min-h-screen text-center transition-colors duration-500">
       {/* if want to make bg animated, add animated-bg to className above and enable it in  */}
       {/* className="min-h-screen flex flex-col items-center justify-center transition-colors duration-500 animated-bg" */}
-      <h1 className="font-[Poppins] text-5xl font-bold font-style: italic">Welcome to</h1>
       <h1 className="font-[Honk] text-[120px]">Ping of Pongs</h1>
-	  <h2 className="text-2xl font-bold">An adaptation of the classic Atari game Pong.</h2>
-	  <img src={ pongImg } alt="A photo of our game" className="rounded-xl shadow-lg w-1/3"/>
-	  <p>Sign up to create a player profile, or sign in if you already have one.</p>
+	    <h2 className="text-2xl font-bold">An adaptation of the classic Atari game Pong.</h2>
+	    <img src={ pongImg } alt="A photo of our game" className="rounded-xl shadow-lg w-1/3"/>
+	    <p>Sign up to create a player profile, or sign in if you already have one.</p>
       <div className="space-x-4">
         <Button to="/signup" variant="secondary">
           Sign Up
@@ -20,13 +19,13 @@ export default function Landing() {
         <Button to="/signin" variant="primary">
           Sign In
         </Button>
-		<Button to="/testing">
-			Testing
-		</Button>
-    {<Button to="/game">
-      Game
-    </Button>}
-    </div>
+        <Button to="/testing">
+          Testing
+        </Button>
+        <Button to="/game">
+          Game
+        </Button>
+      </div>
       <ThemeToggle />
     </div>
   );
