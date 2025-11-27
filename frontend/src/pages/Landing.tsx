5a46c3f992fea83ed7b886c7bc40bb945e85c92a
mariaaallotar <maria.helenius99@gmail.com>
2025-11-27 15:44:14 +0200

    Fixed gaps on landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -7,10 +7,11 @@ import NavBar from "../components/NavBar.tsx";
 
 export default function Landing() {
   return (
-    <div className="flex flex-col gap-8 items-center min-h-screen text-center transition-colors duration-500">
+    <div className="flex flex-col gap-15 items-center min-h-screen text-center transition-colors duration-500">
       {/* if want to make bg animated, add animated-bg to className above and enable it in  */}
       {/* className="min-h-screen flex flex-col items-center justify-center transition-colors duration-500 animated-bg" */}
-      <h1 className="font-press-start text-7xl text-red-700 dark:text-[#e3cc98] tracking-[-0.1em]">Ping of Pongs</h1>	    <img src={ pongImg } alt="A photo of our game" className="rounded-xl shadow-lg w-1/3"/>
+      <h1 className="mt-4 font-press-start text-7xl text-red-700 dark:text-[#e3cc98] tracking-[-0.1em]">Ping of Pongs</h1>
+      <img src={ pongImg } alt="A photo of our game" className="rounded-xl shadow-lg w-1/3"/>
       <div className="space-x-4 flex align-center">
         <Button to="/signup" variant="primary">
           <IoMdPersonAdd className="text-red-700" /> Sign Up
