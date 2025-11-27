72a35f988095925bd1b6e0afa230116cc534f8d4
Ethan <ethanberko@gmail.com>
2025-11-27 13:41:58 +0200

    added signup icon

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,6 +1,8 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { Button } from "../components/Button";
 import pongImg from "../assets/pong-thumbnail.png";
+import { IoMdPersonAdd } from "react-icons/io";
+import Game from "./Game.tsx";
 
 export default function Landing() {
   return (
@@ -8,9 +10,9 @@ export default function Landing() {
       {/* if want to make bg animated, add animated-bg to className above and enable it in  */}
       {/* className="min-h-screen flex flex-col items-center justify-center transition-colors duration-500 animated-bg" */}
       <h1 className="font-press-start text-7xl text-red-700 dark:text-[#e3cc98] tracking-[-0.1em]">Ping of Pongs</h1>	    <img src={ pongImg } alt="A photo of our game" className="rounded-xl shadow-lg w-1/3"/>
-      <div className="space-x-4">
+      <div className="space-x-4 flex align-center">
         <Button to="/signup" variant="secondary">
-          Sign Up
+          <IoMdPersonAdd className="text-red-700" /> Sign Up
         </Button>
         <Button to="/signin" variant="primary">
           Sign In
