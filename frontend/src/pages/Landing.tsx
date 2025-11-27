4e77cde233d5aaaff0912538c13c1d52acf9368b
Ethan <ethanberko@gmail.com>
2025-11-27 13:12:45 +0200

    removed subtitled from landing page

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -7,10 +7,7 @@ export default function Landing() {
     <div className="flex flex-col gap-8 p-16 items-center min-h-screen text-center transition-colors duration-500">
       {/* if want to make bg animated, add animated-bg to className above and enable it in  */}
       {/* className="min-h-screen flex flex-col items-center justify-center transition-colors duration-500 animated-bg" */}
-      <h1 className="font-press-start text-7xl text-red-700 dark:text-[#e3cc98] tracking-[-0.1em]">Ping of Pongs</h1>
-	    <h2 className="text-2xl font-bold">An adaptation of the classic Atari game Pong.</h2>
-	    <img src={ pongImg } alt="A photo of our game" className="rounded-xl shadow-lg w-1/3"/>
-	    <p>Sign up to create a player profile, or sign in if you already have one.</p>
+      <h1 className="font-press-start text-7xl text-red-700 dark:text-[#e3cc98] tracking-[-0.1em]">Ping of Pongs</h1>	    <img src={ pongImg } alt="A photo of our game" className="rounded-xl shadow-lg w-1/3"/>
       <div className="space-x-4">
         <Button to="/signup" variant="secondary">
           Sign Up
