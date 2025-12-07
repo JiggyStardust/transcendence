bf65c5addc12fa601a7f70f1d585afc6331f1445
mariaaallotar <maria.helenius99@gmail.com>
2025-12-07 14:04:59 +0200

    Custom selection color

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-		<div className='bg-light-bg dark:bg-dark-bg text-stone-900 dark:text-stone-200 min-h-screen accent-vintage-red dark:accent-vintage-yellow'>
+		<div className='bg-light-bg dark:bg-dark-bg text-stone-900 dark:text-stone-200 min-h-screen accent-vintage-red dark:accent-vintage-yellow selection:bg-vintage-yellow/70 dark:selection:bg-vintage-red/30'>
 	    <App />
 		</div>
   </StrictMode>
