6d79e32e893b75ccc637d4993f5d3e90ff397c5f
Ethan <ethanberko@gmail.com>
2025-12-09 15:10:14 +0200

    added buttons for games, text that displays player names and matchups, and shuffling players

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,10 +6,10 @@ import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-		<div className='bg-light-bg dark:bg-dark-bg text-black dark:text-white'>
-    <AuthProvider>
-			<App />
-		</AuthProvider>	
-	</div>
-  </StrictMode>,
+    <div className='bg-light-bg dark:bg-dark-bg text-black dark:text-white min-h-screen accent-vintage-red dark:accent-vintage-yellow'>
+      <AuthProvider>
+        <App />
+      </AuthProvider>	
+    </div>
+  </StrictMode>
 )
--- frontend/src/pages/Tournament.tsx
+++ frontend/src/pages/Tournament.tsx
@@ -1,11 +1,60 @@
 
-export default function Tournament(){
+// @ts-nocheck
+
+
+import { Button } from "../components/Button.tsx";
+import { useState } from "react";
+
+export interface User {
+  username: string; // not sure if we need this at all
+}
+
+const Match = ({game_number, player_1, player_2, active=true}: {game_number: string, player_1: string, player_2:string, active: boolean}) => {
   return (
-    <div className="flex justify-center">
-
-      <h1 className="mt-4 font-press text-7xl text-vintage-red dark:text-vintage-yellow [word-spacing:-30px] tracking-[-0.1em]">Tournament</h1>
-
+    <div className="flex justify-center gap-7">
+      <Button variant="primary" size="lg" disabled={!active}>{game_number}</Button>
+      <p className="text-3xl">{player_1} vs. {player_2}</p>
     </div>
+  )
+}
+
+export default function Tournament() {
+  const [players, setPlayers] = useState<User[]>([
+    { username: "Alice" },
+    { username: "Bob" },
+    { username: "Charlie" },
+    { username: "Dana" }
+  ]);
+  const [winner_1, setWinner_1] = useState("Winner of Game 1");
+  const [winner_2, setWinner_2] = useState("Winner of Game 2");
+
+  players.sort( () => Math.random()-0.5 );
+
+
+  
+  return (
+    <div className="flex justify-center flex-col items-center gap-10">
+      <h1 className="mt-4 font-press text-7xl text-vintage-red dark:text-vintage-yellow [word-spacing:-30px] tracking-[-0.1em]">Tournament</h1>
+      <div className="flex flex-col items-start gap-4">
+        <Match
+          game_number="Game 1"
+          player_1={players[0].username}
+          player_2={players[1].username}
+        />
+        <Match
+          game_number="Game 2"
+          player_1={players[2].username}
+          player_2={players[3].username}
+        />
+        <Match
+          game_number="Game 3"
+          player_1={winner_1}
+          player_2={winner_2}
+          active={false}
+        />
+      </div>
+    </div>
+    
   )
 
 }
