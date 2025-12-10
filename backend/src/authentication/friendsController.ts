6521ca96609b062b9c63b51cee6044b37264d47d
Natalie Meintjes <natalie.mein@gmail.com>
2025-12-10 17:03:34 +0200

    some edits to friend controller - incomplete

--- backend/src/authentication/friendsController.ts
+++ backend/src/authentication/friendsController.ts
@@ -1,1 +1,23 @@
 import { FastifyInstance } from "fastify";
+import { FriendService } from "../friendsService";
+
+export class FriendController {
+    private service: FriendService;
+
+    constructor(fastify: FastifyInstance) {
+        this.service = new FriendService(fastify);
+    }
+
+    sendRequest = async (req: any, reply: any) => {
+        const userId = req.user.id;
+        const { toUserId } = req.body;
+
+        try {
+            const res = await this.service.sendRequest(userId, toUserId);
+            return reply.send(res);
+        } catch (err: any) {
+            return reply.badRequest(err.message);
+        }
+    };
+}
+
