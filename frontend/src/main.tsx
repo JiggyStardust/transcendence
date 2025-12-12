ab6df08a3c6d19f9ef2c66f905f9c9c8876ce17a
mariaaallotar <maria.helenius99@gmail.com>
2025-12-12 14:17:48 +0200

    Toast now shows an status message for 5 sec in the top right of the page, should work on any page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from "./context/AuthContext";
+import { ToastProvider } from "./context/ToastContext";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
 		<div className='bg-light-bg dark:bg-dark-bg text-black dark:text-white min-h-screen accent-vintage-red dark:accent-vintage-yellow'>
 	    <AuthProvider>
-				<App />
+				<ToastProvider>
+					<App />
+				</ToastProvider>
 			</AuthProvider>	
 		</div>
   </StrictMode>
