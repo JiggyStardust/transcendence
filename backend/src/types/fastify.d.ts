6a21e84a4dd2bb4637da6ad3e309222a60a336a4
Julia Llama <julia.persidskaia@gmail.com>
2025-12-15 01:44:22 +0200

    Remove fastify authenticate deorator

--- backend/src/types/fastify.d.ts
+++ backend/src/types/fastify.d.ts
@@ -10,12 +10,6 @@ declare module "fastify" {
   }
 }
 
-declare module "fastify" {
-  interface FastifyInstance {
-    authenticate(request: FastifyRequest, reply: FastifyReply): Promise<void>;
-  }
-}
-
 declare module "@fastify/jwt" {
   interface FastifyJWT {
     user: {
