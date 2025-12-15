7ff30f287fa7b58bbd369abff3fcfa8fcf1894e0
Natalie Meintjes <natalie.mein@gmail.com>
2025-12-15 16:54:39 +0200

    still working on create match stuff

--- backend/src/database/utils/createMatch.ts
+++ backend/src/database/utils/createMatch.ts
@@ -11,42 +11,26 @@ import {err, ok} from "../types";
  * @returns DbResult with new entry in the match table or error "SOME_ERROR"
  */
 
-// I'm not sure exactly how we are going to collect the data
-// Frontend collects
-/*const gameState = {
-  startedAt,
-  endedAt,
-  winnerUserId,
-  participants: [
-    { userId: 7, score: 11, startedAt, endedAt },
-    { userId: 12, score: 8, startedAt, endedAt }
-  ]
-};
 
-// Backend receives
-await prisma.match.create({
-  data: {
-    startedAt: gameState.startedAt,
-    endedAt: gameState.endedAt,
-    winnerId: gameState.winnerUserId,
-    participants: {
-      create: gameState.participants
-    }
-  }
-});*/
 
 export const createMatch = async (
   prisma: PrismaClient,
   userIds: number[], // participants
+  scores: number[],
+  winner: boolean[],
 ): Promise<DbResult<IMatchData>> => { 
   try {
     if (userIds.length < 2) {
         return err("NOT_ENOUGH_PARTICIPANTS")
     }
+    if (userIds.length !== scores.length || userIds.length !== winner.length) {
+        return err("INVALID_MATCH_PAYLOAD");
+    }
 
+    const now = new Date();
+    const winnerId = userIds[winnerIndexes[0]];
     const match = await prisma.match.create({
       data: {
-        startedAt: new Date(),
         participants: {
             create: userIds.map((id) => ({ userId: id }))
         }
