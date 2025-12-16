514f8b4b8f09f7e51fe18492c10ad4b3480fb015
mariaaallotar <maria.helenius99@gmail.com>
2025-12-16 12:07:21 +0200

    Login now opens a modal to input Two Factor Authentication if enabled

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,11 +8,50 @@ import { useAuth } from "../context/AuthContext";
 import { useAppToast } from "../context/ToastContext";
 import { type Status } from "../types/types";
 
+interface TwoFAModalProps {
+  twoFACode: string;
+  twoFACodeStatus: Status | null;
+  handleTwoFAChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  validateTwoFA: () => Promise<void>;
+  onClose: () => void;
+}
+
+const TwoFAModal = ({ twoFACode, onClose, twoFACodeStatus, handleTwoFAChange, validateTwoFA}: TwoFAModalProps) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center backdrop-blur-sm bg-black/50 z-50">
+      <div className="bg-light-bg dark:bg-dark-bg p-8 rounded-xl shadow-xl max-w-sm w-full text-center flex flex-col items-center gap-2">
+        <h3 className="text-md font-semibold mb-4">Please write the code from your Two Factor Authentication app</h3>
+        <div className="flex flex-col gap-4 mb-4">
+          <Input
+            id="twoFactorAuthToken"
+            label="Two Factor Authentication code"
+            value={twoFACode}
+            autofocus={true}
+            status={twoFACodeStatus || undefined}
+            tooltip="Open the app that you used for two factor authentication and input the code from there"
+            onChange={handleTwoFAChange}/>
+        </div>
+        <div className="flex gap-39">
+          <Button variant="secondary" onClick={onClose}>
+            Close
+          </Button>
+          <Button variant="primary" disabled={twoFACode === ""} onClick={validateTwoFA}>
+            Verify
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function Login() {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [usernameStatus, setUsernameStatus] = useState<Status | null>(null);
   const [passwordStatus, setPasswordStatus] = useState<Status | null>(null);
+  const [twoFAModalOpen, setTwoFAModalOpen] = useState(false);
+  const [twoFACode, setTwoFACode] = useState("");
+  const [twoFACodeStatus, setTwoFACodeStatus] = useState<Status | null>(null);
 
   const navigate = useNavigate();
   const { login, isAuthenticated } = useAuth();
@@ -36,14 +75,57 @@ export default function Login() {
 
     const data = await res.json();
 
+    console.log("handle login: " + data.twoFARequired);
+
     if (!res.ok) {
       showToast("Login failed: " + data.error, "error");
       setUsername("");
       setPassword("");
       return;
     }
+    if (data.twoFARequired) {
+      console.log("Two factor required");
+      setTwoFAModalOpen(true);
+    } else {
+      login();
+      navigate("/dashboard");
+    }
+  }
+
+  const handleTwoFAChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const code = e.target.value
+    setTwoFACode(code);
+    if (code === "") {
+      setTwoFACodeStatus({type: "error", message: "Can not be empty"});
+    } else {
+      setTwoFACodeStatus(null);
+    }
+  }
+
+  async function validateTwoFA () {
+
+    console.log("Validate clicked");
+    const token = twoFACode;
+
+    const res = await fetch(PROXY_URL + "/login-2fa", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({ username, token })
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      showToast("Validation failed: " + data.error, "error");
+      return;
+    }
     login();
     navigate("/dashboard");
+  }
+
+  function onClose() {
+    setTwoFAModalOpen(false);
   }
 
   function handleUsernameChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -91,6 +173,9 @@ export default function Login() {
           />
           <Button disabled={ username === "" || password === "" }>Login</Button>
         </form>
+        {twoFAModalOpen && (
+          <TwoFAModal twoFACode={twoFACode} onClose={onClose} twoFACodeStatus={twoFACodeStatus} handleTwoFAChange={handleTwoFAChange} validateTwoFA={validateTwoFA} />
+        )}
       </div>
     </div>
   );
