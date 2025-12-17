0e72699449c6641dac59b7d125c94505e655f54b
Natalie Meintjes <natalie.mein@gmail.com>
2025-12-17 12:48:50 +0200

    changed the match history route so it no longer has a query

--- backend/src/routes/getMatchHistory.ts
+++ backend/src/routes/getMatchHistory.ts
@@ -5,14 +5,9 @@ import { verifyToken } from "../authentication/authMiddleware";
 export default async function matchHistory(fastify: FastifyInstance) {
     fastify.get("/match_history", { preHandler: verifyToken }, async (req, reply) => {
         try {
-            const userId = Number((req.query as { userId?: string }).userId);
+            const userId = req.user.id;
 
-            //validation
-            if (!userId) {
-                return reply.code(400).send({ error: " Missing required param: userId" });
-            }
-
-            // get match histor using bd function
+            // get match history using bd function
             const result = await getUserMatches(fastify.db, userId);
 
             if (result.ok) {
